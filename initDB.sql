ff2798347dc27f62d4a894350fe4048b539676fd
Vaka-vaka <Waka2006@ukr.net>
2021-10-29 15:21:52 +0300

    Update initDB.sql

--- initDB.sql
+++ initDB.sql
@@ -1,1 +1,13 @@
+create table Developers (
+	id serial PRIMARY KEY,
+	name varchar(50) not null,
+	age integer not null,
+	gender varchar(50) not null
+);
 
+
+create table Skills(
+	id serial PRIMARY KEY,
+	language varchar(250) not null,
+	level_skills integer not null 	
+);
