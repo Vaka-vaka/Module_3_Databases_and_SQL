19671d2e26594465a4652e72e04ebf23844ca25d
Vaka-vaka <Waka2006@ukr.net>
2021-10-30 18:43:12 +0300

    Update initDB.sql

--- initDB.sql
+++ initDB.sql
@@ -29,3 +29,35 @@ create table Customers(
 	name varchar(64) not null,
 	city varchar(100) not null
 );
+
+create table developers_to_skills(
+	dev_id integer not null,
+	skills_id integer not null,
+	
+	constraint dev_id_fk foreign key(dev_id) references Developers(id),
+	constraint skills_id_fk foreign key(skills_id) references Skills(id)
+);
+
+create table developers_to_projects(
+	dev_id integer not null,
+	projects_id integer not null,
+	
+	constraint dev_id_fk foreign key(dev_id) references Developers(id),
+	constraint projects_id_fk foreign key(projects_id) references Projects(id)
+);
+
+create table companies_to_projects(
+	companies_id integer not null,
+	projects_id integer not null,
+	
+	constraint companies_id_fk foreign key(companies_id) references Companies(id),
+	constraint projects_id_fk foreign key(projects_id) references Projects(id)
+);
+
+create table customers_to_projects(
+	customers_id integer not null,
+	projects_id integer not null,
+	
+	constraint customers_id_fk foreign key(customers_id) references Customers(id),
+	constraint projects_id_fk foreign key(projects_id) references Projects(id)
+);
